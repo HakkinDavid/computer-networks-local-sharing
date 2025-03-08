ad14b3f442ea4d448d52ef6dd370b5ea280abcf0
HakkinDavid <desr11@hotmail.com>
2025-03-07 19:19:13 -0800

    attempt

---
+++ client_bt.py
@@ -0,0 +1,31 @@
+import bluetooth
+import sys
+
+server_mac = sys.argv[1] if len(sys.argv) > 1 else input("Enter server MAC address: ")
+port = 1
+
+client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+client_sock.connect((server_mac, port))
+print("Connected to server")
+
+while True:
+    command = input("Enter command (LIST, DOWNLOAD <file>, UPLOAD <file>, EXIT): ")
+    client_sock.send(command.encode())
+    
+    if command == "LIST":
+        data = client_sock.recv(1024).decode()
+        print("Files on server:\n" + data)
+    elif command.startswith("DOWNLOAD "):
+        filename = command.split(" ")[1]
+        with open(filename, "wb") as f:
+            f.write(client_sock.recv(1024))
+        print("Download completed")
+    elif command.startswith("UPLOAD "):
+        filename = command.split(" ")[1]
+        with open(filename, "rb") as f:
+            client_sock.sendall(f.read())
+        print("Upload completed")
+    elif command == "EXIT":
+        break
+
+client_sock.close()
---
+++ server_bt.py
@@ -0,0 +1,49 @@
+from pybluez import bluetooth
+import os
+
+DIRECTORY = "server_files"
+if not os.path.exists(DIRECTORY):
+    os.makedirs(DIRECTORY)
+
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+server_sock.bind(("", bluetooth.PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+bluetooth.advertise_service(server_sock, "FileTransferServer",
+                            service_classes=[bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE])
+
+print(f"Bluetooth server listening on port {port}")
+
+client_sock, client_info = server_sock.accept()
+print(f"Accepted connection from {client_info}")
+
+def handle_client(sock):
+    while True:
+        data = sock.recv(1024).decode()
+        if not data:
+            break
+        if data == "LIST":
+            files = "\n".join(os.listdir(DIRECTORY))
+            sock.send(files.encode())
+        elif data.startswith("DOWNLOAD "):
+            filename = data.split(" ")[1]
+            filepath = os.path.join(DIRECTORY, filename)
+            if os.path.exists(filepath):
+                with open(filepath, "rb") as f:
+                    sock.sendall(f.read())
+            else:
+                sock.send(b"ERROR: File not found")
+        elif data.startswith("UPLOAD "):
+            filename = data.split(" ")[1]
+            filepath = os.path.join(DIRECTORY, filename)
+            with open(filepath, "wb") as f:
+                f.write(sock.recv(1024))
+            sock.send(b"UPLOAD SUCCESS")
+        elif data == "EXIT":
+            break
+    sock.close()
+
+handle_client(client_sock)
+server_sock.close()
