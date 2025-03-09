9d757514846b2e4a9aea1906eb10b70c6a20a17c
HakkinDavid <desr11@hotmail.com>
2025-03-09 00:00:34 -0800

    Update server_bt.py

--- server_bt.py
+++ server_bt.py
@@ -7,6 +7,7 @@ if not os.path.exists(DIRECTORY):
 
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 server_sock.bind(("", bluetooth.PORT_ANY))
+server_mac = bluetooth.read_local_bdaddr()[0]
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
@@ -14,7 +15,7 @@ bluetooth.advertise_service(server_sock, "FileTransferServer",
                             service_classes=[bluetooth.SERIAL_PORT_CLASS],
                             profiles=[bluetooth.SERIAL_PORT_PROFILE])
 
-print(f"Bluetooth server listening on port {port}")
+print(f"Bluetooth server listening on MAC {server_mac}, port {port}")
 
 client_sock, client_info = server_sock.accept()
 print(f"Accepted connection from {client_info}")
